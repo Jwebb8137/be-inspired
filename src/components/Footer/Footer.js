866e885a3021f29f37c2a5dd2af7dfca84dcd427
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-12-23 21:47:36 -0800

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,6 @@ import Logo from '../../images/icon.jpg'
 const Footer = () => {
   return (
     <footer className="page-footer">
-      <div className="icon-container">
-        <i className="fab fa-facebook"></i>
-        <i className="fab fa-instagram"></i>
-        <i className="fab fa-twitter"></i>
-      </div>
       <div className="footer-copyright">© 2020 Copyright:
         <a href="/"> BeInspired.com</a>
         <img id="footer-logo" src={Logo} />
